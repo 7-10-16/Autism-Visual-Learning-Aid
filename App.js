2365b92b82827b6c73a6d0cae7863f4447f183dd
n.j.gillespie@2019.ljmu.ac.uk <nathan@tyrenet.net>
2022-11-25 10:32:59 +0000

    create export file for TTS components and more TTS on menus

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as Speech from 'expo-speech';
 
 import { styles } from './config/componentColourPalette.js';
+import {TTSText, Say} from "./Components/TTS.js";
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FruitScreen from './screens/FruitScreen';
 import AnimalScreen from './screens/AnimalScreen';
@@ -15,103 +16,22 @@ import ProfileScreen from './screens/ProfileScreen';
 //Starting screen of app FOR NOW
 function HomeScreen({ navigation }) {
 
-  // available voices {
-//   "identifier": "com.apple.ttsbundle.siri_Gordon_en-AU_compact",
-//   "identifier": "com.apple.voice.compact.en-AU.Karen",
-//   "identifier": "com.apple.ttsbundle.siri_Catherine_en-AU_compact",
-//   "identifier": "com.apple.eloquence.en-GB.Rocko",
-//   "identifier": "com.apple.eloquence.en-GB.Shelley",
-//   "identifier": "com.apple.voice.compact.en-GB.Daniel",
-//   "identifier": "com.apple.ttsbundle.siri_Martha_en-GB_compact",
-//   "identifier": "com.apple.eloquence.en-GB.Grandma",
-//   "identifier": "com.apple.eloquence.en-GB.Grandpa",
-//   "identifier": "com.apple.eloquence.en-GB.Flo",
-//   "identifier": "com.apple.eloquence.en-GB.Eddy",
-//   "identifier": "com.apple.eloquence.en-GB.Reed",
-//   "identifier": "com.apple.eloquence.en-GB.Sandy",
-//   "identifier": "com.apple.ttsbundle.siri_Arthur_en-GB_compact",
-//   "identifier": "com.apple.voice.compact.en-IE.Moira",
-//   "identifier": "com.apple.voice.compact.en-IN.Rishi",
-//   "identifier": "com.apple.eloquence.en-US.Flo",
-//   "identifier": "com.apple.speech.synthesis.voice.Bahh",
-//   "identifier": "com.apple.speech.synthesis.voice.Albert",
-//   "identifier": "com.apple.speech.synthesis.voice.Fred",
-//   "identifier": "com.apple.speech.synthesis.voice.Hysterical",
-//   "identifier": "com.apple.speech.synthesis.voice.Organ",
-//   "identifier": "com.apple.speech.synthesis.voice.Cellos",
-//   "identifier": "com.apple.speech.synthesis.voice.Zarvox",
-//   "identifier": "com.apple.eloquence.en-US.Rocko",
-//   "identifier": "com.apple.eloquence.en-US.Shelley",
-//   "identifier": "com.apple.speech.synthesis.voice.Princess",
-//   "identifier": "com.apple.eloquence.en-US.Grandma",
-//   "identifier": "com.apple.eloquence.en-US.Eddy",
-//   "identifier": "com.apple.speech.synthesis.voice.Bells",
-//   "language": "en-US",
-//   "identifier": "com.apple.eloquence.en-US.Grandpa",
-//   "identifier": "com.apple.speech.synthesis.voice.Trinoids",
-//   "identifier": "com.apple.speech.synthesis.voice.Kathy",
-//   "identifier": "com.apple.eloquence.en-US.Reed",
-//   "identifier": "com.apple.speech.synthesis.voice.Boing",
-//   "identifier": "com.apple.speech.synthesis.voice.Whisper",
-//   "identifier": "com.apple.speech.synthesis.voice.Deranged",
-//   "identifier": "com.apple.speech.synthesis.voice.GoodNews",
-//   "identifier": "com.apple.ttsbundle.siri_Nicky_en-US_compact",
-//   "identifier": "com.apple.speech.synthesis.voice.BadNews",
-//   "identifier": "com.apple.ttsbundle.siri_Aaron_en-US_compact",
-//   "identifier": "com.apple.speech.synthesis.voice.Bubbles",
-//   "identifier": "com.apple.voice.compact.en-US.Samantha",
-//   "identifier": "com.apple.eloquence.en-US.Sandy",
-//   "identifier": "com.apple.speech.synthesis.voice.Junior",
-//   "identifier": "com.apple.speech.synthesis.voice.Ralph",
-//   "identifier": "com.apple.voice.compact.en-ZA.Tessa",
-// }
-  // Example of how to use the speak method from Speech
-  // const speak = () => {
-  //   const thingToSay = '1';
-  //   Speech.speak(thingToSay);
-  // };
-
-  // Component to display a touchable text field with text to speech capability
-  // params{ 
-  // phrase (what to say with Speech)
-  // text (what text is displayed) 
-  // }
-  class TTSText extends Component {
-    render() {
-      return (
-        <TouchableOpacity onPress={Speech.speak(this.props.phrase, {voice: "com.apple.ttsbundle.siri_Catherine_en-AU_compact"})}>
-          <Text style={this.props.style} onPress={()=>Speech.speak(this.props.phrase, {voice: "com.apple.ttsbundle.siri_Catherine_en-AU_compact"}) } >{this.props.text}</Text>
-        </TouchableOpacity>
-      )
-    };
-  }
-
-  // Component to only use text to speech
-  class Say extends Component {
-    render() {
-      return (
-          <Text onPress={Speech.speak(this.props.phrase, {voice: "com.apple.ttsbundle.siri_Catherine_en-AU_compact"})} />
-      )
-    };
-  }
-
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-
 
       <TTSText style={{ fontSize: 26, fontWeight: 'medium'}} text="Welcome Back" phrase="Welcome Back"/>
       <TTSText style={{ fontSize: 26, fontWeight: 'medium', color: '#00cc00'}} text="Nathan!" phrase="Nathan!"/>
 
       <Say phrase="What would you like to do?"/>
-            <TouchableOpacity style ={styles.lightButton} onPress = {() => {navigation.navigate('Categories')}}>
+            <TouchableOpacity style ={styles.lightButton} onPress = {() => {Speech.speak("Quiz"); navigation.navigate("Categories")}}>
             <TTSText style={{ fontSize: 26, fontWeight: 'medium', color: '#00cc00'}} text="Quiz" phrase="take a quiz?"/>
                 <Ionicons name='library-outline' size={50} style = {styles.darkIcon}></Ionicons>
             </TouchableOpacity>
 
-            {/* <Say phrase="Or"/> */}
+            <Say phrase="Or"/>
             
             <View style={{flexDirection: 'row', marginTop: '10%'}}>
-                <TouchableOpacity style ={styles.darkButton} onPress = {() => {navigation.navigate('Profile')}}>
+                <TouchableOpacity style ={styles.darkButton} onPress = {() => {Speech.speak("Profile"); navigation.navigate("Profile") }}>
                 <TTSText style={{ fontSize: 26, fontWeight: 'medium', color: 'white'}} text="Profile" phrase="View your profile"/>
                     <Ionicons name='ios-person-circle-outline' size={50} style = {styles.lightIcon}></Ionicons>
                 </TouchableOpacity>
@@ -124,20 +44,23 @@ function CategoriesScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start', backgroundColor:'#003f5c'}}>
 
-      <TouchableOpacity style ={styles.fruitButton} onPress = {() => {navigation.navigate('Fruit')}}>
-                <Text style={styles.buttonText}>Fruit</Text>
-                  <Ionicons name='nutrition' size={50} style = {styles.catIcon}></Ionicons>
+      <Say phrase="What category of quiz?"/>
+
+      <TouchableOpacity style ={styles.fruitButton} onPress = {() => {Speech.speak("Fruit Quiz"); navigation.navigate("Fruit")}}>
+                <TTSText style={styles.buttonText} text="Fruit" phrase="Fruit"/>
+                <Ionicons name='nutrition' size={50} style = {styles.catIcon}></Ionicons>
       </TouchableOpacity>
-      <TouchableOpacity style ={styles.animalButton} onPress = {() => {navigation.navigate('Animal')}}>
-                <Text style={styles.buttonText}>Animal</Text>
+      <TouchableOpacity style ={styles.animalButton} onPress = {() => {Speech.speak("Animal Quiz"); navigation.navigate("Animal")}}>
+          <TTSText style={styles.buttonText} text="Animal" phrase="Animal"/>
                   <Ionicons name='paw' size={50} style = {styles.catIcon}></Ionicons>
       </TouchableOpacity>
-      <TouchableOpacity style ={styles.randomButton} onPress = {() => {navigation.navigate('Random')}}>
-                <Text style={styles.buttonText}>Random</Text>
+      <TouchableOpacity style ={styles.randomButton} onPress = {() => {Speech.speak("Random Quiz"); navigation.navigate("Random")}}>
+      <TTSText style={styles.buttonText} text="Random" phrase="Random"/>
                 <Ionicons name='help' size={50} style = {styles.catIcon}></Ionicons>
       </TouchableOpacity>
-      <TouchableOpacity style ={styles.cHomeButton} onPress = {() => {navigation.navigate('Home')}}>
-                <Text style={styles.buttonText}>Home</Text>
+      <Say phrase="Or..."></Say>
+      <TouchableOpacity style ={styles.cHomeButton} onPress = {() => {Speech.speak("Home"); navigation.navigate('Home')}}>
+                <TTSText style={styles.buttonText} text="Home" phrase="Home"/>
                    <Ionicons name='home' size={50} style = {styles.catIcon}></Ionicons>
       </TouchableOpacity>
     </View>
@@ -145,7 +68,7 @@ function CategoriesScreen({ navigation }) {
 }
 
 
-function HistoryScreen({ navigation }) {
+function HistoryScreen({ navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>History Screen</Text>
