a794fb3b5b8e946724c4be7290df0dc6b78fd123
7-10-16 <77738624+7-10-16@users.noreply.github.com>
2022-11-29 16:37:27 +0000

    fixed

--- screens/ExampleScreen.js
+++ screens/ExampleScreen.js
@@ -8,7 +8,6 @@ export default function ExampleScreen({ navigation }) {
     const [name, onChangeName] = React.useState("");
     const [age, onChangeAge] = React.useState(0);
     const [users, setUsers] = React.useState([]);
-    const [fruits, setFruits] = React.useState([]);
 
     const addUser = () => {
         axios.post('https://node-server-udw2.onrender.com/create', 
@@ -24,13 +23,6 @@ export default function ExampleScreen({ navigation }) {
         axios.get('https://node-server-udw2.onrender.com/users').then((response) => {
             console.log(response.data);
             setUsers(response.data);
-        });
-    }
-
-    const getFruits = () => {
-        axios.get('https://node-server-udw2.onrender.com/fruits').then((response) => {
-            console.log(response.data);
-            setFruits(response.data);
         });
     }
 
@@ -54,16 +46,16 @@ export default function ExampleScreen({ navigation }) {
                 color="#841584"
             />
             <Button
-                onPress={getFruits}
+                onPress={getUsers}
                 title="Show Users"
                 color="#841584"
             />
             <Text>
-                {fruits.map(fruit => {
+                {users.map(user => {
                     return (
                         //Each child in a list should have a unique "key" prop.
-                        <li key={fruit.ID}> 
-                            {fruit.name} - {fruit.fruit_age} 
+                        <li key={user.user_ID}> 
+                            {user.user_fname} - {user.user_age} 
                         </li>
                         )
                 })}
