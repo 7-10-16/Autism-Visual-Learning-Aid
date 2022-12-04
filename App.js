9c7be314df9edb8a2c72bc88ea87c3a2916c8694
7-10-16 <77738624+7-10-16@users.noreply.github.com>
2022-12-04 16:47:01 +0000

    changes to the navigation and header appearances

--- App.js
+++ App.js
@@ -28,7 +28,6 @@ function HomeScreen({ navigation }) {
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <TTSText style={{ fontSize: 26, fontWeight: "medium" }} phrase="Welcome back" text="Welcome back"/>
       <TTSText style={{ fontSize: 26, fontWeight: "medium", color: "#00cc00" }} phrase="USERNAME!" text="USERNAME!"/>
-
       <Say phrase="What would you like to do?"/>
 
       {/* Quiz Navigation Button */}
@@ -63,21 +62,6 @@ function HomeScreen({ navigation }) {
             name="ios-person-circle-outline"
             size={50}
             style={styles.lightIcon}
-          />
-        </TouchableOpacity>
-
-        {/* Example Screen Navigation Button */}
-        <TouchableOpacity
-          style={styles.lightButton}
-          onPress={() => {
-            navigation.navigate("Example"); Speech.stop(); Speech.speak("Example");
-          }}
-        >
-          <Text style={{ margin: 10, fontSize: 40 }}>Example</Text>
-          <Ionicons
-            name="library-outline"
-            size={50}
-            style={styles.darkIcon}
           />
         </TouchableOpacity>
       </View>
@@ -167,7 +151,11 @@ function App() {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LogIn} />
         <Stack.Screen name="Register" component={Reg} />
+        <Stack.Group
+          screenOptions={{ headerShown: false, gestureEnabled: false }}
+        >
         <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Group>
         <Stack.Screen name="Categories" component={CategoriesScreen} />
         <Stack.Screen name="Example" component={ExampleScreen} />
         <Stack.Group
